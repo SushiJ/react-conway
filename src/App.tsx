65f5046494dc26f578e9d9a1e212dd6d0cc098b8
Sushant Sharma <sushant.wurk@gmail.com>
2023-11-05 00:06:17 +0530

    working

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,29 @@
 import { useCallback, useRef, useState } from "react";
 import { produce } from "immer";
-import "./App.css";
 
 const ROWS = 50;
 const COLS = 50;
 
+const DIRECTIONS = [
+  [0, 1],
+  [0, -1],
+  [1, 0],
+  [-1, 0],
+  [1, 1],
+  [1, -1],
+  [-1, 1],
+  [-1, -1],
+];
+function generateEmptyGrid() {
+  const rows = [];
+  for (let i = 0; i < ROWS; ++i) {
+    rows.push(Array.from(Array(COLS), () => 0));
+  }
+  return rows;
+}
+
 function App() {
+  const [running, setRunning] = useState<boolean>(false);
   const [grid, setGrid] = useState(() => {
     const rows = [];
 
@@ -15,19 +33,57 @@ function App() {
     return rows;
   });
 
-  const [running, setRunning] = useState(false);
   const runningRef = useRef(running);
   runningRef.current = running;
 
   const runSim = useCallback(() => {
-    if (!runningRef) return;
+    if (!runningRef.current) return;
 
-    setTimeout(runSim, 1000);
+    setGrid((g) => {
+      return produce(g, (gCopy) => {
+        for (let i = 0; i < ROWS; ++i) {
+          for (let j = 0; j < COLS; ++j) {
+            let neighbors = 0;
+            DIRECTIONS.forEach(([x, y]) => {
+              const newI = i + x;
+              const newJ = j + y;
+
+              if (newI >= 0 && newI < ROWS && newJ >= 0 && newJ < COLS) {
+                neighbors += g[newI][newJ];
+              }
+            });
+            if (neighbors < 2 || neighbors > 3) gCopy[i][j] = 0;
+            else if (g[i][j] === 0 && neighbors === 3) gCopy[i][j] = 1;
+          }
+        }
+      });
+    });
+
+    setTimeout(runSim, 100);
   }, []);
+
   return (
-    <>
-      <button onClick={() => setRunning(!running)}>
+    <div className="container" style={{ maxWidth: "1010px" }}>
+      <button
+        className="outline"
+        onClick={() => {
+          setRunning(!running);
+          if (!running) {
+            runningRef.current = true;
+            runSim();
+          }
+        }}
+      >
         {running ? "Stop" : "Start"}
+      </button>
+      <button
+        className="secondary outline"
+        onClick={() => {
+          setGrid(generateEmptyGrid);
+          setRunning(false);
+        }}
+      >
+        Reset
       </button>
       <div
         style={{
@@ -39,7 +95,7 @@ function App() {
         }}
       >
         {grid.map((rows, i) =>
-          rows.map((col, k) => (
+          rows.map((_, k) => (
             <div
               key={`${i}-${k}`}
               onClick={() => {
@@ -51,14 +107,14 @@ function App() {
               style={{
                 width: 20,
                 height: 20,
-                backgroundColor: grid[i][k] ? "pink" : undefined,
-                border: "solid 1px black",
+                backgroundColor: grid[i][k] ? "#d81b60" : undefined,
+                border: "dashed 1px #5b6d80",
               }}
             ></div>
           )),
         )}
       </div>
-    </>
+    </div>
   );
 }
 
